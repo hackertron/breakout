b22392351f7a436e547f4b10ab7698f8e82de8e2
hackertron <cooljay.gupta@gmail.com>
2017-12-13 23:10:45 +0530

    initial change

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,18 +1,16 @@
-/* globals __DEV__ */
+
 import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+
 
 export default class extends Phaser.State {
   init () {}
   preload () {}
 
   create () {
-    
+
   }
 
   render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
-    }
+  
   }
 }
