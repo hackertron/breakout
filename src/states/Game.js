2ee4c0a9a886d4ec1d5b4d9e3ea912a2fd8a621c
hackertron <cooljay.gupta@gmail.com>
2017-12-13 23:07:51 +0530

    initial change

--- src/states/Game.js
+++ src/states/Game.js
@@ -7,23 +7,7 @@ export default class extends Phaser.State {
   preload () {}
 
   create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
-
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
-
-    this.game.add.existing(this.mushroom)
+    
   }
 
   render () {
